5ca983035824fcff00d282612833cf195ea89bc1
Aarti Srinivasan <aartisrinivasan83@gmail.com>
2021-08-27 16:00:49 -0700

    Add files via upload

---
+++ Strings/main.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        char_map = {}
+        max_length = 0
+        start = 0
+
+        for end, end_char in enumerate(s):
+            if end_char in char_map and char_map[end_char] >= start:
+                start = char_map[end_char] + 1
+            char_map[end_char] = end
+            current = end - start + 1
+            max_length = max(max_length, current)
+
+        return max_length
+
