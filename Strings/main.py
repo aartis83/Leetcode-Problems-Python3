5d3e0cc7d4b4ebc5f117a7738160f0c3911b637f
Aarti Srinivasan <aartisrinivasan83@gmail.com>
2021-08-27 15:59:27 -0700

    Add files via upload

---
+++ Strings/main.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s):
+
+        import re
+        s = re.sub(r'[\W_]', '', s).lower()
+
+        left , right = 0, len(s) - 1
+
+        while left <= right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
+
+
+
