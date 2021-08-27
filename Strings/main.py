c4d867f054e36698796e3cc75cf20badcb9c05e3
Aarti Srinivasan <aartisrinivasan83@gmail.com>
2021-08-27 16:02:10 -0700

    Add files via upload

---
+++ Strings/main.py
@@ -0,0 +1,23 @@
+# Given a string s, find the longest palindromic substring in s
+# --- Example
+# longestPalindrome("cbbd") --> "bb"
+# longestPalindrome("abba") --> "abba"
+# longestPalindrome("a") --> "a"
+class Solution:
+    def longestPalindrome(self, s):
+       res = ""
+
+       for i in range(len(s)):
+           current = self.expandMiddle(s, i - 1, i + 1)
+           in_between = self.expandMiddle(s, i, i + 1)
+           res = max(res, current, in_between, key=len)
+
+       return res
+
+    def expandMiddle(selfself, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return s[left + 1 : right]
+
+
