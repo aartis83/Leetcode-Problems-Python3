b6b87caa033dc34fa0d43ecbe9388d1761574e87
Aarti Srinivasan <aartisrinivasan83@gmail.com>
2021-08-27 15:57:06 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s):
+        stack = []
+        pairs = {'{': '}', '[': ']', '(' : ')'}
+
+        for char in s:
+            if char in pairs:
+                stack.append(char)
+            elif len(stack) == 0 or pairs[stack.pop()] != char:
+                return False
+        return len(stack) == 0
+
+
+
+
